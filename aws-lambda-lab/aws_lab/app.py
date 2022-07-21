76d106c52373d7c0749df1b2acd1dc85315b4c45
jrm2087 <jrm2087@gmail.com>
2022-07-20 21:48:28 -0500

    ajustes lambda

--- aws-lambda-lab/aws_lab/app.py
+++ aws-lambda-lab/aws_lab/app.py
@@ -1,7 +1,8 @@
-import boto3
+import os
 import json
+import csv
 import logging
-import pandas as pd
+import boto3
 
 s3 = boto3.client('s3')
 logger = logging.getLogger('lab-aws-lambda')
@@ -15,12 +16,34 @@ def lambda_handler(event, context):
     file_key = event['Records'][0]['s3']['object']['key']
     logger.info(f'Reading {file_key} from {bucket_name}')
     obj = s3.get_object(Bucket=bucket_name, Key=file_key)
+    name, extension = os.path.splitext(str(file_key))
+    logger.info(f'name -> {name}')
+    logger.info(f'extension -> {extension}')
+
+    if str(file_key).find('/') >= 0:
+        logger.error(str(file_key).find('/'))
+        logger.error('No es la ruta raiz')
+        return
+
+    # obtemos contenido archivo json
     file_content = obj['Body'].read().decode('utf-8')
     data_json = json.loads(file_content)
 
-    # convertimos json a csv y movemos a data
-    name = str(file_key).split('.')
-    df = pd.DataFrame.from_dict(data_json)
-    s3.put_object(Bucket=bucket_name, Body=df.to_csv(index=False, header=True), Key=f'data/{name[0]}.csv')
+    # generamos archivo para escribir
+    data_file = open('/tmp/data.csv', 'w', newline='', encoding='utf-8')
+    csv_writer = csv.writer(data_file)
 
-    return None
+    count = 0
+    for data in data_json:
+        if count == 0:
+            header = data.keys()
+            csv_writer.writerow(header)
+            count += 1
+        csv_writer.writerow(data.values())
+    data_file.close()
+
+    # Leemos el archivo y lo ponemos en la ruta del bucket
+    with open('/tmp/data.csv', 'r', encoding='utf-8') as data_csv:
+        s3.put_object(Bucket=bucket_name, Body=data_csv.read(), Key=f'data/{name}.csv')
+
+    return
